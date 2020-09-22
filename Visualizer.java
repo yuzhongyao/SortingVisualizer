e25ba74454dfd514f7dfa2dd1224eb8357255d2e
yuzhongyao <60209555+yuzhongyao@users.noreply.github.com>
2020-09-22 01:36:41 -0400

    Create Visualizer.java

---
+++ Visualizer.java
@@ -0,0 +1,54 @@
+package SortingAlgorithms;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+
+public class Visualizer {
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 1200, HEIGHT = WIDTH / 12 * 9;
+	private Thread thread;
+	private boolean running = false;
+	
+	public Visualizer () {
+		Window w = new Window(WIDTH, HEIGHT, "Visualize");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
